caf979b09eaca008c10b94305a036ee0aeae5ea6
alexcravalho <a.d.cravalho@gmail.com>
2019-10-11 21:02:42 -0700

    adhering to Airbnb styleguide;

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
-const webpack = require('webpack');
-const src = path.join(__dirname, '/client/src')
-const dist = path.join(__dirname, '/client/src/dist')
+
+const src = path.join(__dirname, '/client/src');
+const dist = path.join(__dirname, '/client/src/dist');
 
 
 module.exports = {
-	mode: 'development',
-	entry: `${src}/index.jsx`,
+  mode: 'development',
+  entry: `${src}/index.jsx`,
 
-	output: {
-		filename: 'bundle.js',
-		path: dist
-	},
+  output: {
+    filename: 'bundle.js',
+    path: dist,
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules)/,
-				include: src,
-				loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        include: src,
+        loader: 'babel-loader',
 
-				options: {
-					plugins: [],
+        options: {
+          plugins: [],
 
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						],
-						'@babel/preset-react'
-					]
-				}
-			}
-		]
-	},
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+        },
+      },
+    ],
+  },
 
-	devServer: {
-		open: true
-	}
+  devServer: {
+    open: true,
+  },
 };
